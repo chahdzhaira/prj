603a5d91a83bb26da8a7af1d9f4d172851989d06
chahdzhaira <chahd.zhaira@esprit.tn>
2023-12-08 13:02:30 +0100

    spring_salaht_bloc

---
+++ src/main/java/tn/esprit/archwork/entities/Bloc.java
@@ -0,0 +1,32 @@
+package tn.esprit.archwork.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Bloc {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idBloc;
+    String nomBloc;
+    Long capaciteBloc;
+    @JsonIgnore
+    @ManyToOne
+    private Foyer foyer;
+    //@OneToMany(mappedBy = "bloc" ,fetch = FetchType.Lazy,cascade =  CascadeType.ALL)
+    @OneToMany(cascade = CascadeType.ALL,mappedBy = "bloc")
+    Set<Chambre> chambres;
+
+
+}
